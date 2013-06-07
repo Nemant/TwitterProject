65582e98af340a30c404fcc231be3d5c5f5ae14c
Danish Khan <danishkhan@server.fake>
2013-06-07 02:37:21 +0100

    need eclipsify

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,9 +3,7 @@ package controllers;
 import play.data.validation.Constraints.Required;
 import static play.data.Form.*;
 import models.Metrics;
-
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -17,9 +15,20 @@ public class Application extends Controller {
 	public static Result index() {
 		return ok(index.render(form(TweetID.class), Metrics.getVolumeOfTweets(), Metrics.getVolumeOfReTweets(), Metrics.getTotalretweetsForTweet(), Metrics.getTotalretweetsForUser(), Metrics.getAvgretweetsForUser()));
 	}
+	
+	public static Result liveFeed() {
+	      Integer answer = new Integer(6);
+	      return ok(ajax_result.render(answer));
+	  }
 
-	public static void generate() {
-
-	}
+	public static Result javascriptRoutes() {
+	    response().setContentType("text/javascript");
+	    return ok(
+	      Routes.javascriptRouter("jsRoutes",
+	        // Routes
+	        controllers.routes.javascript.Application.liveFeed()
+	      )
+	    );
+	  }
 
 }
