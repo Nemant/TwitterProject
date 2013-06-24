c6904efba6a97b4fa2de57019284e08a68b670ca
Danish Khan <danishkhan@server.fake>
2013-06-24 02:45:53 +0100

    Live

--- app/models/QueryDB.java
+++ app/models/QueryDB.java
@@ -50,7 +50,7 @@ public class QueryDB {
 			if (workingSet.equals(EnumDataSet.TWEETS)) {
 				preparedStatement = connection2.prepareStatement("SELECT \"UniqueTweets\" FROM \"FinalProject\".\"TweetsInInterval\" WHERE \"DateTimeStart\" < '2013-03-21 00:06:00+01' ORDER BY \"DateTimeFinish\" ASC");
 			} else if (workingSet.equals(EnumDataSet.RETWEETS)) {
-				preparedStatement = connection2.prepareStatement("SELECT \"UniqueReTweets\" FROM \"FinalProject\".\"ReTweetsInInterval\" WHERE \"DateTimeStart\" < '2013-02-21 00:06:00+01' ORDER BY \"DateTimeFinish\" ASC");
+				preparedStatement = connection2.prepareStatement("SELECT \"UniqueReTweets\" FROM \"FinalProject\".\"ReTweetsInInterval\" WHERE \"DateTimeStart\" < '2013-03-21 00:06:00+01' ORDER BY \"DateTimeFinish\" ASC");
 			}
 			
 			rs = preparedStatement.executeQuery();
